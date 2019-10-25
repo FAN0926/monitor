e364379035d04e7ae36ce1a2f775b20a13143f2d
Pinef <fanzheseng@gmail.com>
2019-10-25 22:19:16 +0800

    实现函数的封装，实现对文件夹下的所有有时间文件的视频进行分析，得出有物体移动的时间段，根据时间段进行视频的剪辑和合并

---
+++ cut.py
@@ -0,0 +1,139 @@
+# -*-coding=utf-8 -*-
+
+"""
+Author: Pinef
+Email: fan0926@bupt.edu.cn
+date: 2019/10/22 17:06
+desc: 
+"""
+
+import time
+from tqdm import tqdm
+import subprocess
+import os
+import time_analysis
+
+
+def cut_video(start_time, during_time, input, output):
+    '''
+    构造ffmpeg的切割语句，利用subprocess运行cmd语句
+    :param start_time:
+    :param during_time:
+    :param input:
+    :param output:
+    :return:
+    '''
+    cmd = [
+        'ffmpeg',
+        '-ss',
+        start_time,
+        '-t',
+        during_time,
+        '-accurate_seek',
+        '-i',
+        input,
+        '-codec',
+        'copy',
+        output]
+    subprocess.call(cmd)
+
+
+def concat_video(filelistpath, output):
+    '''
+    构造ffmpeg的合并连接语句，利用subprocess运行cmd语句
+    :param filelistpath:
+    :param output:
+    :return:
+    '''
+    cmd = [
+        'ffmpeg',
+        '-f',
+        'concat',
+        '-safe',
+        '0',
+        '-i',
+        filelistpath,
+        '-c',
+        'copy',
+        output]
+    subprocess.call(cmd)
+
+
+def start(src_folder, endwith):
+    '''
+    对文件夹内的所有视频文件进行分析，并根据分析结果进行剪辑合并
+    :param src_folder:
+    :param endwith:
+    :return:
+    '''
+    src_paths = [os.path.join(src_folder, path) for path in os.listdir(src_folder) if path.endswith(endwith)]
+    print('src_paths:', src_paths)
+    for i in tqdm(src_paths):  # 开始对每个文件夹下的每个视频文件进行剪辑
+        txt_path = i.replace(endwith, '.txt')  # 读取该文件对应的时间文件
+        out_file_list = []                    # 切割后的视频文件路径
+        if os.path.exists(txt_path):
+            get_time_list = time_analysis.analysis(txt_path, 5)       # 对时间文件进行分析，找到出现移动目标的时间段，
+            # print(get_time_list)                                    # 允许空白时间5s
+            for a in range(int(len(get_time_list) / 2)):   # 根据时间段对视频进行剪切
+                start_time = get_time_list.pop(0)
+                new_start_time = "Sat Mar 28 {} 2019".format(start_time)
+                end_time = "Sat Mar 28 {} 2019".format(get_time_list.pop(0))
+                strftime1 = time.mktime(time.strptime(new_start_time, "%a %b %d %H:%M:%S %Y"))  # 格式化为时间戳
+                strftime2 = time.mktime(time.strptime(end_time, "%a %b %d %H:%M:%S %Y"))
+                during_time = str(int(strftime2 - strftime1))
+                cut_video(start_time, during_time, i, i.replace(endwith, '_{}'.format(a)+endwith))  # 对该段进行剪辑
+                out_file_list.append(i.replace(endwith, '_{}'.format(a)+endwith))
+
+            with open('fileline.txt', 'w+', encoding='utf-8') as f:
+                for file in out_file_list:
+                    f.write("file '{}'".format(file))
+                    f.write('\n')
+            concat_video('fileline.txt', txt_path.replace('.txt', '_fin'+endwith))
+
+        else:
+            print("txt_path:", txt_path, '不存在，请检查。')
+            continue
+
+
+if __name__ == '__main__':
+    start(r'F:\OneDrive - bupt.edu.cn\PycharmProjects\python_foundation\Opencv', '.mp4')
+
+    # import subprocess
+    #
+    # sp = subprocess.Popen('ping 127.0.0.1 -n 100', stdout=subprocess.PIPE)
+    # while True:
+    #     line = sp.stdout.readline()
+    #     if not line: break
+    #     print(line)
+    # if sp.wait() == 0:
+    #     print('sub process execute ok')
+
+    # import subprocess
+    #
+    #
+    # class NetDetect:
+    #
+    #     def __init__(self, addr):
+    #         self.addr = addr
+    #         self.log = open('NetDetect.log', 'w')
+    #         if self.log == None:
+    #             print('Open NetDetect.log failed')
+    #
+    #     def __del__(self):
+    #         if self.fdp:
+    #             self.fdp.kill()
+    #         if self.log:
+    #             self.log.close()
+    #
+    #     def ping(self):
+    #         self.fdp = subprocess.Popen(['ping', self.addr, '-t'], stdout=subprocess.PIPE)
+    #         for line in self.fdp.stdout:
+    #             print(line.decode("gb2312"))
+    #             self.log.write(line.decode("gb2312"))
+    #             self.log.flush()
+    #         self.fdp = None
+    #
+    #
+    # if __name__ == '__main__':
+    #     nd = NetDetect('www.sina.com.cn')
+    #     nd.ping()
--- time_analysis.py
+++ time_analysis.py
@@ -20,51 +20,48 @@ def mulsq(type,times):   # 为了消除噪声，构造连续列表
     tmp = [type] * times
     return tmp
 
-def split_time(time_sq):
+
+def split_time(time_sq):   # 切割最后输出的时间列表使其成对出现
     start_time = time_sq.pop(0)
     end_time = time_sq.pop(0)
     return start_time,end_time
 
-if __name__ == '__main__':
-    papa = pd.read_csv(r'F:\OneDrive - bupt.edu.cn\PycharmProjects\python_foundation\Opencv\result\A ISVID_20191012_191644_1.txt', sep='\t',
+
+def analysis(vedio_txt_path, max_empty_time, Rrawpic=False):
+    papa = pd.read_csv(vedio_txt_path, sep='\t',
                        delimiter='\s+')  # 加载papa.txt,指定它的分隔符是 \t
-    papa.head()  # 显示数据的前几行
-    rowNum = papa.shape[0]  # 不包括表头
-    colNum = papa.columns.size
-    #profile = pandas_profiling.ProfileReport(papa)
-    #profile.to_file("outputfile.html")
-    #print(papa,rowNum,colNum)
-    count ={'1':[],'0':[]}
+    # papa.head()  # 显示数据的前几行
+    # rowNum = papa.shape[0]  # 不包括表头
+    # colNum = papa.columns.size
+    # profile = pandas_profiling.ProfileReport(papa)  # pandas的一个初步的自动的数据分析库
+    # profile.to_file("outputfile.html")              # 调用库的输出可以保存为一个html文件
+    count = {'1': [], '0': []}
     result = []
     for k, v in itertools.groupby(list(papa['flag'])):
-        #print(type(k),len(list(v)),count[str(k)].get(str(len(list(v)))))
+        # print(type(k),len(list(v)),count[str(k)].get(str(len(list(v)))))
         times = len(list(v))
         count[str(k)].append(len(list(v)))
         if k == 1:
-            result.extend(mulsq(k,times))
+            result.extend(mulsq(k, times))
         if k == 0:
-            if times<=60:   # 设定容许的连零个数
+            if times <= max_empty_time:  # 设定容许的连零个数
                 result.extend(mulsq(1, times))
             else:
                 result.extend(mulsq(0, times))
 
-    # print(count)
-    # print(count.get('1'))
-    # print(count.get('0'))
     papa['new_flag'] = result
-    #print(papa.head())
     get_time = []
     flag = False  # 默认一开始是没有检测到移动对象的，即数据为0
-    for row in papa.itertuples(index=True, name='Pandas'):
-        print(getattr(row, "time"), getattr(row, "new_flag"))
+    for row in tqdm(papa.itertuples(index=True, name='Pandas')):
+        # print(getattr(row, "time"), getattr(row, "new_flag"))
         tmp_time = getattr(row, "time")
         tmp_flag = getattr(row, "new_flag")
         if flag is False:  # 若处于未检测到的状态
-            if tmp_flag == 1:   # 出现移动物体，添加此刻时间作为该段开始时间，并设置信号为已检测到的状态
+            if tmp_flag == 1:  # 出现移动物体，添加此刻时间作为该段开始时间，并设置信号为已检测到的状态
                 get_time.append(tmp_time)
                 flag = True
                 pass
-            if tmp_flag == 0:   # 未出现移动物体，读取下一秒的数据
+            if tmp_flag == 0:  # 未出现移动物体，读取下一秒的数据
                 pass
         if flag is True:
             if tmp_flag == 1:
@@ -72,15 +69,13 @@ if __name__ == '__main__':
             if tmp_flag == 0:
                 get_time.append(tmp_time)
                 flag = False
-    if len(get_time)%2 != 0:
-        get_time.append(papa.iat[-1,0])  # 如果获取到的时间不是成对成对的，添加文件的最后一行的第一列的时间
-    print(get_time)
+    if len(get_time) % 2 != 0:
+        get_time.append(papa.iat[-1, 0])  # 如果获取到的时间不是成对成对的，添加文件的最后一行的第一列的时间
+    # print(get_time)
     copy_get_time = get_time.copy()
-    for i in range(int(len(get_time)/2)):  # 切割时间使其成对出现
-        print(split_time(copy_get_time))
-
-
-
+    for i in range(int(len(get_time) / 2)):  # 切割时间使其成对出现
+        # print(split_time(copy_get_time))
+        pass
 
     # matplotlib.rcParams['font.sans-serif'] = ['SimHei']
     # matplotlib.rcParams['font.family']='sans-serif'
@@ -92,16 +87,21 @@ if __name__ == '__main__':
     # plt.ylabel('flag')
     #
     # plt.show()
+    if Rrawpic:
+        matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+        matplotlib.rcParams['font.family']='sans-serif'
+        plt.plot(papa.time, result, 'ro-', color='#4169E1', alpha=0.8, label='一些数字')
+        plt.xticks(papa.time, papa.time, rotation=90)  # 这里是调节横坐标的倾斜度，rotation是度数
+        # 显示标签，如果不加这句，即使加了label='一些数字'的参数，最终还是不会显示标签
+        plt.legend(loc="upper right")
+        plt.xlabel('time')
+        plt.ylabel('flag')
+        for i in range(int(len(get_time))):  # 切割时间使其成对出现
+            #print(split_time(get_time))
+            plt.plot(get_time[i], 1, 'r-o')
+        plt.show()
 
-    matplotlib.rcParams['font.sans-serif'] = ['SimHei']
-    matplotlib.rcParams['font.family']='sans-serif'
-    plt.plot(papa.time, result, 'ro-', color='#4169E1', alpha=0.8, label='一些数字')
-    plt.xticks(papa.time, papa.time, rotation=90)  # 这里是调节横坐标的倾斜度，rotation是度数
-    # 显示标签，如果不加这句，即使加了label='一些数字'的参数，最终还是不会显示标签
-    plt.legend(loc="upper right")
-    plt.xlabel('time')
-    plt.ylabel('flag')
-    for i in range(int(len(get_time))):  # 切割时间使其成对出现
-        #print(split_time(get_time))
-        plt.plot(get_time[i], 1, 'r-o')
-    plt.show()
+    return get_time
+
+if __name__ == '__main__':
+    analysis(r'F:\OneDrive - bupt.edu.cn\PycharmProjects\python_foundation\Opencv\result\test.txt', 2, False)
